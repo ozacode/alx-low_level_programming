00a6cf1565f897c85376d0d8acd61374ca6c88b9
ivan <rismozart3@gmail.com>
2022-09-11 12:05:12 -0700

    positive or negative

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,63 +1,31 @@
-#include <stdio.h>                                               
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
 
-                                                                 
-
-                                                                 
-
-                                                                 
-
-/**                                                              
-
-                                                                 
-
-*main - print if the number is positive, zero, or negative       
-
-*                                                                
-
-*Description: using the main function                            
-
-*this program prints "Programming is positive, zeo, or negative  
-
-*Return: 0                                                       
-
-*/                                                               
-
-int main(void)                                                   
-
-{                                                                
-
-int n;                                                           
-
-srand(time(0));                                                  
-
-n= rand() -RAND_MAX /2;                                          
-
-/* your code goes there */                                       
-
-if (n > 0)                                                       
-
-{                                                                
-
-printf("%1 is positive\n", n);                                   
-
-}                                                                
-
-else if (n == 0)                                                 
-
-{    print("%1 is zero\n",n);                                     
-
-}                               
-
-                                                                
-
-else if (n < 0)                                                  
-
-{                                                                
-
-printf ("%1 is negative\n", n);                                  
-
-}                                                                
-
-return (0);                                                      
-
-}   
+/**
+*main - print if the number is positive, zero, or negative
+*
+*Description: using the main function
+*this program prints "Programming is positive, zero, or negative
+*Return: 0
+*/
+int main(void)
+{
+int n;
+srand(time(0));
+n = rand() - RAND_MAX / 2;
+/* your code goes there */
+if (n > 0)
+{
+printf("%i is positive\n", n);
+}
+else if (n == 0)
+{
+printf("%i is zero\n", n);
+}
+else if (n < 0)
+{
+printf("%i is negative\n", n);
+}
+return (0);
+}
