86fc9f0ed8c75f856433a9273d7a9e3e8e1df13d
ivan <rismozart3@gmail.com>
2022-09-19 21:59:52 -0700

    fixed

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -1,19 +1,10 @@
 #include <stdio.h>
-
-
-
 /**
-
 * main - print if the number is positive, zero, or negative
-
 *
-
 * Description: using the main function
-
 * this program prints "Programming is positive, zeor, or negative
-
 * Return: 0
-
 */
 
 int main(void)
