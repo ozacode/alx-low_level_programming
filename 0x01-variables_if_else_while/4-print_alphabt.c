986d4657837405317f53c2873c74b4e2f51b1dbc
ivan <rismozart3@gmail.com>
2022-09-19 22:07:00 -0700

    fixed

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -6,28 +6,16 @@
 * this program prints "Programming is positive, zeor, or negative
 * Return: 0
 */
-
 int main(void)
-
 {
-
-char ch;
-
-for (ch = 'a' ; ch <= 'z' ; ch++)
-
-{
-
-if (ch != 'e' && ch != 'q')
-
-{
-
-putchar(ch);
-
+	char ch;
+	for (ch = 'a' ; ch <= 'z' ; ch++)
+	{
+		if (ch != 'e' && ch != 'q')
+		{
+			putchar(ch);
+		}
+	}
+	putchar('\n');
+	return (0);
 }
-
-}
-
-putchar('\n');
-
-return (0);
-
