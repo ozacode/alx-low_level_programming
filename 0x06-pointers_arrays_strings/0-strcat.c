7fa8e6c5c674606fe3aa16c501c1c54f3143f6c9
ivan <rismozart3@gmail.com>
2022-09-22 03:36:02 -0700

    fixed

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -7,15 +7,15 @@
 *
 * Return: returns poiner to @dest
 */
-char *_strcat(char *dest, char *src)
+char *_strcat(char *dest, const char *src)
 {
 	int index = 0, dest_len = 0;
 
 	while (dest[index++])
-	dest_len++
+		dest_len++;
 
 	for (index = 0; src[index]; index++)
-	dest[dest_len++] = src[index];
-
+		dest[dest_len++] = src[index];
+	
 	return (dest);
 }
