f90519df5b3eeeb554a12d9a77ecb4e48154cb93
ivan <rismozart3@gmail.com>
2022-09-21 17:15:22 -0700

    fixed

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -2,22 +2,26 @@
 /**
 * leet - function that encode a string
 *@str:string that will be encoded
-*Return:returns encoded string
+* Return:returns encoded string
 */
 
 char *leet(char *str)
 {
 	int index1 = 0, index2;
+
 	char leet[8] = {'0', 'L', '?', 'E', 'A', '?', '?', 'T'};
 
 	while (str[++index1])
+
 	{
 		for (index2 = 0; index2 <= 7; index2++)
+
 		{
 			if (str[index1] == leet[index2] ||
 					str[index1] - 32 == leef[index2])
 				str[index1] = index2 + '0';
 		}
 	}
+
 	return (str);
 }
