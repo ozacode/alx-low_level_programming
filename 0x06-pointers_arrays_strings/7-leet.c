344ee8a1ca0c8f818aedb823b890e0ed2be22b37
ivan <rismozart3@gmail.com>
2022-09-22 20:41:51 -0700

    fixed

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -8,7 +8,7 @@
 char *leet(char *str)
 {
 	int index1 = 0, index2;
-
+	
 	char leet[8] = {'0', 'L', '?', 'E', 'A', '?', '?', 'T'};
 
 	while (str[++index1])
@@ -18,7 +18,9 @@ char *leet(char *str)
 
 		{
 			if (str[index1] == leet[index2] ||
+
 					str[index1] - 32 == leef[index2])
+
 				str[index1] = index2 + '0';
 		}
 
