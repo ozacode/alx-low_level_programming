0e2274c329316e72cdc815f7b40d4dd38458c30d
ivan <rismozart3@gmail.com>
2022-09-23 22:42:04 -0700

    fixed

--- 0x06-pointers_arrays_strings/103-infinite_add.c
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -1,4 +1,5 @@
 #include "main.h"
+
 /**
  * infinite_add - adds two numbers
  * @n1: first number
@@ -9,20 +10,17 @@
  * Return: address of r or 0
  */
 char *infinite_add(char *n1, char *n2, char *r, int size_r)
-
 {
 	int i, j, k, l, m, n;
-	
+
 	for (i = 0; n1[i]; i++)
 		;
 	for (j = 0; n2[j]; j++)
 		;
 	if (i > size_r || j > size_r)
 		return (0);
-
 	m = 0;
 	for (i -= 1, j -= 1, k = 0; k < size_r - 1; i--, j--, k++)
-
 	{
 		n = m;
 		if (i >= 0)
@@ -40,7 +38,6 @@ char *infinite_add(char *n1, char *n2, char *r, int size_r)
 	if (i >= 0 || j >= 0 || m)
 		return (0);
 	for (k -= 1, l = 0; l < k; k--, l++)
-
 	{
 		m = r[k];
 		r[k] = r[l];
@@ -48,3 +45,4 @@ char *infinite_add(char *n1, char *n2, char *r, int size_r)
 	}
 	return (r);
 }
+
