dd7b1a1123dfb9e1ba2f56800d64c8f750d69dc0
ivan <rismozart3@gmail.com>
2022-09-24 02:28:41 -0700

    fixed

--- 0x04-more_functions_nested_loops/100-prime_factor.c
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -1,52 +1,32 @@
-#include "main.h"                                                                                                                       
-                                                                                                                                        
-char *add_strings(char *n1, char *n2, char *r, int r_index);                                                                            
-char *infinite_add(char *n1, char *n2, char *r, int size_r);                                                                            
-                                                                                                                                        
-/**                                                                                                                                     
- * add_strings - Adds the numbers stored in two strings.                                                                                
- * @n1: The string containing the first number to be added.                                                                             
- * @n2: The string containing the second number to be added.                                                                            
- * @r: The buffer to store the result.                                                                                                  
- * @r_index: The current index of the buffer.                                                                                           
- *                                                                                                                                      
- * Return: If r can store the sum - a pointer to the result.                                                                            
- *         If r cannot store the sum - 0.                                                                                               
- */                                                                                                                                     
-char *add_strings(char *n1, char *n2, char *r, int r_index)                                                                             
-{                                                                                                                                       
-        int num, tens = 0;                                                                                                                                
-        
-        for (; *n1 && *n2; n1--, n2--, r_index--)                                                                                       
-        {                                                                                                                               
-                num = (*n1 - '0') + (*n2 - '0');                                                                                        
-                num += tens;                                                                                                            
-                *(r + r_index) = (num % 10) + '0';                                                                                      
-                tens = num / 10;                                                                                                        
-        }                                                                                                                               
-                                                                                                                                        
-        for (; *n1; n1--, r_index--)                                                                                                    
-        {                                                                                                                               
-                num = (*n1 - '0') + tens;                                                                                               
-                *(r + r_index) = (num % 10) + '0';                                                                                      
-                tens = num / 10;                                                                                                        
-        }                                                                                                                               
-                                                                                                                                        
-        for (; *n2; n2--, r_index--)                                                                                                    
-        {                                                                                                                               
-                num = (*n2 - '0') + tens;                                                                                               
-                *(r + r_index) = (num % 10) + '0';                                                                                      
-                tens = num / 10;                                                                                                        
-        }                                                                                                                               
-                                                                                                                                        
-        if (tens && r_index >= 0)                                                                                                       
-        {                                                                                                                               
-                *(r + r_index) = (tens % 10) + '0';                                                                                     
-                return (r + r_index);                                                                                                   
-        }                                                                                                                               
-                                                                                                                                        
-        else if (tens && r_index < 0)                                                                                                   
-                return (0);                                                                                                             
-                                                                                                                                        
-        return (r + r_index + 1);                                                                                                       
+#include <stdio.h>
+
+/**
+*main- prints the largest prime factor
+*of a number
+*
+*Return: returns 0
+*/
+
+int main(void)
+{
+	long number = 612852475143;
+	int inc;
+
+	while (inc++ < number / 2)
+	{
+	if (number % inc == 0)
+	{
+		number /= 2;
+		continue;
+	}
+
+	for (inc = 3; inc < number / 2; inc += 2)
+	{
+		if (number % inc == 0)
+			number /= inc;
+	}
+	}
+	printf("%ld\n", number);
+	return (0);
 }
+
