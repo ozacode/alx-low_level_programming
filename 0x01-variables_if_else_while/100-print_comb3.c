ef93fa11d394e87e6da2dbeead9d0fcd72b683b8
ivan <rismozart3@gmail.com>
2022-09-23 20:43:41 -0700

    fixed

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -7,27 +7,28 @@
 
 int main(void)
 {
-	int i, j;
+        int i, j;
 
-	for (i = 0; i <= 9; i++)
-	{
-		for (j = 1; j <= 9; i++)
-		{
-			if (j > i)
-			{
-				putchar(i + '0');
-				putchar(j + '0');
+        for (i = 0; i <= 9; i++)
+        {
+                for (j = 1; j <= 9; i++)
+                {
+                        if (j > i)
+                        {
+                                putchar(i + '0');
+                                putchar(j + '0');
 
-				if (i != 8)
-				{
-					putchar(',');
-					putchar(' ');
-				}
-			}
-		}
-	}
-	putchar('\n');
+                                if (i != 8)
+                                {
+                                        putchar(',');
+                                        putchar(' ');
+                                }
+                        }
+                }
+        }
+        putchar('\n');
 
-	return (0);
+        return (0);
 
 }
+
