a7aa50335dfa7f2a5baf792116bde551333a27bd
ivan <rismozart3@gmail.com>
2022-09-27 15:39:24 -0700

    fixed

--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -1,5 +1,10 @@
-int _putchar(char c);
-void *_memset(void *s, int c, size_t n);
+#ifndef MAIN_H
+#define MAIN_H
+
+/**
+*Auth: ozacode
+*/
+char *_memset(char *s, char b, unsigned int n);
 char *_memcpy(char *dest, char *src, unsigned int n);
 char *_strchr(char *s, char c);
 unsigned int _strspn(char *s, char *accept);
@@ -8,3 +13,7 @@ char *_strstr(char *haystack, char *needle);
 void print_chessboard(char (*a)[8]);
 void print_diagsums(int *a, int size);
 void set_string(char **s, char *to);
+int _putchar(char c);
+
+#endif /*MAIN_H*/
+
