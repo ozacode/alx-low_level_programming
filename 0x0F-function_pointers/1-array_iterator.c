a3fc5dfc4b6d6f03a0b90ace9e5b9e7cecb3d694
ivan <rismozart3@gmail.com>
2022-10-14 11:00:59 -0700

    fixed

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -9,12 +9,12 @@
 *
 * Return: no return.
 */
+
 void array_iterator(int *array, size_t size, void (*action)(int))
 {
 	unsigned int i;
-
 	if (array && action)
 		for (i = 0; i < size; i++)
- 			action(array[i]);
+			action(array[i]);
 
 }
