44698cb6a0aba73f332bb269d8e2066fe90128b4
ivan <rismozart3@gmail.com>
2022-10-14 09:12:30 -0700

    fixed

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -3,7 +3,7 @@
 #include <stdarg.h>
 
 /**
-* Auth: Ozacode M
+* Auth: ivan
 * print_numbers - Prints numbers, followed by a new line.
 * @separator: The string to be printed between numbers.
 * @n: The number of integers passed to the function.
